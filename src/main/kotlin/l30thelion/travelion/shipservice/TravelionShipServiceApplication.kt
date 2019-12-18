69ba9a2105a1c2ce5acbdbeaee2f20b3ef530603
LeoDuarte <lduarte@rccl.com>
2019-12-18 12:43:51 -0500

    Initial import

---
+++ src/main/kotlin/l30thelion/travelion/shipservice/TravelionShipServiceApplication.kt
@@ -0,0 +1,11 @@
+package l30thelion.travelion.shipservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TravelionShipServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<TravelionShipServiceApplication>(*args)
+}
---
+++ src/test/kotlin/l30thelion/travelion/shipservice/TravelionShipServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package l30thelion.travelion.shipservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TravelionShipServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
